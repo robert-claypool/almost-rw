c90c5929148b8157493ac63ee7265af55c30ebdb
Robert Claypool <robert.g.claypool+git@gmail.com>
2015-08-29 01:47:13 -0500

    fixed indenting

--- almost.js
+++ almost.js
@@ -6,29 +6,28 @@ var almost = (function() {
     }
     
     return {
-        getWords : function(howMany)
-            {
-                if (window.crypto && window.crypto.getRandomValues) {
-                    // Get random values using a (believed to be) cryptographically secure method.			
-                    // See http://stackoverflow.com/questions/5651789/is-math-random-cryptographically-secure
-                    var array = new Uint32Array(howMany);
-                    window.crypto.getRandomValues(array);
+        getWords : function(howMany) {
+            if (window.crypto && window.crypto.getRandomValues) {
+                // Get random values using a (believed to be) cryptographically secure method.			
+                // See http://stackoverflow.com/questions/5651789/is-math-random-cryptographically-secure
+                var array = new Uint32Array(howMany);
+                window.crypto.getRandomValues(array);
 
-                    // Build our passphrase. The words are space delimited.
-                    var p = '';
-                    for (var i = 0; i < array.length; i++) {
-                        // Choose a corresponding entry from our words list.
-                    var word = lookupWord(array[i]);
-                        p += ' ' + word;
-                    }
-
-                    // TODO: Trim the leading/trailing spaces.
-
-                    return p;
+                // Build our passphrase. The words are space delimited.
+                var p = '';
+                for (var i = 0; i < array.length; i++) {
+                    // Choose a corresponding entry from our words list.
+                var word = lookupWord(array[i]);
+                    p += ' ' + word;
                 }
-                else {
-                    return "Error: Cannot find a cryptographically secure source of randomness."
-                }
+
+                // TODO: Trim the leading/trailing spaces.
+
+                return p;
             }
+            else {
+                return "Error: Cannot find a cryptographically secure source of randomness."
+            }
+        }
     };
 })();
