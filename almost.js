d8e05d33f1a0b7955f191f0d31ec161c6e2fe0e8
Robert Claypool <robert.g.claypool+git@gmail.com>
2015-09-02 17:30:00 -0500

    rework the JS callbacks

--- almost.js
+++ almost.js
@@ -1,10 +1,9 @@
-var almost = (function() {
+var almost = {};
+
+(function() {
     this.wordlist = [];
 
-    var load = function(callback) {
-        var that = this;
-        that.callback = callback;
-
+    almost.load = function(callback) {
         var request = new XMLHttpRequest();
         // Requires a web server; CORS will reject loading this via the file: protocol
         request.open('GET', 'diceware.wordlist.asc', true);
@@ -21,11 +20,11 @@ var almost = (function() {
                     if (line === '-----BEGIN PGP SIGNATURE-----') { break; }
                     var word = /^\d{5}\s(.+)$/.exec(line);
                     if (word) {
-                        that.wordlist.push(word[1]);
+                        wordlist.push(word[1]);
                     }
                 }
 
-                that.callback();
+                callback();
             }
             else {
                 // We reached our target server, but it returned an error
@@ -39,37 +38,33 @@ var almost = (function() {
         request.send();
     }
 
-    return {
-        getWords : function(howMany) {
-            var c = window.crypto || window.msCrypto;
-            if (c && c.getRandomValues) {
-                // Get random values using a (believed to be) cryptographically secure method.
-                // See http://stackoverflow.com/questions/5651789/is-math-random-cryptographically-secure
-                var array = new Uint32Array(/*edge requires explicit type conversion*/Number(howMany));
-                c.getRandomValues(array);
+    almost.getWords = function(howMany) {
+        var c = window.crypto || window.msCrypto;
+        if (c && c.getRandomValues) {
+            // Get random values using a (believed to be) cryptographically secure method
+            // See http://stackoverflow.com/questions/5651789/is-math-random-cryptographically-secure
+            var array = new Uint32Array(/*edge requires explicit type conversion*/Number(howMany));
+            c.getRandomValues(array);
 
-                // Build our passphrase. The words are space delimited.
-                return load(function() {
-                    var p = '';
-                    for (var i = 0; i < array.length; i++) {
-                        // Choose a corresponding entry from our words list.
-                        var word = wordlist[5];
-                        p += ' ' + word;
-                    }
-
-                    // trim() is native to String only in browsers that support ECMAScript 5
-                    if (!String.prototype.trim) {
-                        String.prototype.trim = function() {
-                            return this.replace(/^\s+|\s+$/g,'');
-                        };
-                    }
-
-                    return p.trim();
-                });
+            // The words are space delimited.
+            var p = '';
+            for (var i = 0; i < array.length; i++) {
+                // Grab the corresponding entry from our words list
+                var word = wordlist[8]; // TODO: replace the hard-coded key
+                p += ' ' + word;
             }
-            else {
-                return "Error: Cannot find a cryptographically secure source of randomness."
+
+            // trim() is native to String only in browsers that support ECMAScript 5
+            if (!String.prototype.trim) {
+                String.prototype.trim = function() {
+                    return this.replace(/^\s+|\s+$/g,'');
+                };
             }
+
+            return p.trim();
+        }
+        else {
+            return "Error: Cannot find a cryptographically secure source of randomness."
         }
     };
 })();
