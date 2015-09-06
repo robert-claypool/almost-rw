048badc9579ca0f21fea68441af977f04ce82816
Robert Claypool <robert.g.claypool+git@gmail.com>
2015-09-06 18:30:58 -0500

    constrain scope with 'let'

--- almost.js
+++ almost.js
@@ -1,7 +1,7 @@
+"use strict";
 var almost = {};
 
 (function() {
-    "use strict";
     var wordlist = [];
 
     almost.load = function(callback) {
@@ -11,21 +11,21 @@ var almost = {};
             return;
         }
 
-        var request = new XMLHttpRequest();
+        let request = new XMLHttpRequest();
         // Requires a web server; CORS will reject loading this via the file: protocol
         request.open('GET', 'diceware.wordlist.asc', true);
 
         request.onload = function() {
             if (request.status >= 200 && request.status < 400) {
                 // Extract the words
-                var data = request.responseText;
-                var lines = data.split('\n');
-                for (var i = 0; i < lines.length; i++) {
-                    var line = lines[i];
+                let data = request.responseText;
+                let lines = data.split('\n');
+                for (let i = 0; i < lines.length; i++) {
+                    let line = lines[i];
                     if (line === '-----BEGIN PGP SIGNED MESSAGE-----') { continue; }
                     if (line === '') {  continue; }
                     if (line === '-----BEGIN PGP SIGNATURE-----') { break; }
-                    var word = /^\d{5}\s(.+)$/.exec(line);
+                    let word = /^\d{5}\s(.+)$/.exec(line);
                     if (word) {
                         wordlist.push(word[1]);
                     }
@@ -47,22 +47,22 @@ var almost = {};
     };
 
     almost.getWords = function(howMany) {
-        var c = window.crypto || window.msCrypto;
+        let c = window.crypto || window.msCrypto;
         if (c && c.getRandomValues) {
             // Get random values using a cryptographically sound method
             // See http://stackoverflow.com/questions/5651789/is-math-random-cryptographically-secure
             if (howMany > 1000) { howMany = 1000; }
-            var array = new Uint16Array(/*edge requires explicit type conversion*/Number(howMany));
+            let array = new Uint16Array(/*edge requires explicit type conversion*/Number(howMany));
             c.getRandomValues(array);
 
-            var words = [];
-            var uint16_range = 65535; // 0xFFFF - 0x0000
-            for (var i = 0; i < array.length; i++) {
+            let words = [];
+            const uint16_range = 65535; // 0xFFFF - 0x0000
+            for (let i = 0; i < array.length; i++) {
                 // Get our random number as a percent along the range of possibilities
-                var pct = array[i] / uint16_range;
+                let pct = array[i] / uint16_range;
                 // Scale up for the number of words we have
-                var j = Math.floor(pct * wordlist.length);
-                var word = wordlist[j];
+                let j = Math.floor(pct * wordlist.length);
+                let word = wordlist[j];
                 words.push(word);
             }
 
